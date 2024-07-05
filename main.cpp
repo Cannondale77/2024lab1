89339d51148a3129f0fc5d89e351fad9a0f828c7
Cannondale77 <osullivansg@gmail.com>
2024-07-05 21:16:05 +0000

    comment 3

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 InterruptIn bluebtn(PC_13);
 
-int int_trig = 0;
+volatile int int_trig = 0;
 
 void bttnFall(){
     int_trig = 2;
@@ -19,7 +19,7 @@ int main()
     bluebtn.fall(&bttnFall);
     bluebtn.rise(&bttnRise);
 
-// this is  a program for lab 1 embedded system  module, its my first time coding as big learning 
+// this is  a program for lab 1 embedded system  module, its my first time coding it is a big learning experience
     while (true) {
         switch(int_trig){
         case 1:
